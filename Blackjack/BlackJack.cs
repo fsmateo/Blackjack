e9e2efaf951b44c9633b3a64754d2640d8d91ae1
Brandon Veltre <bveltre@sdsu.edu>
2018-11-29 13:47:50 -0800

    Deleted Hand.cs

--- Blackjack/BlackJack.cs
+++ Blackjack/BlackJack.cs
@@ -20,10 +20,6 @@ namespace Blackjack
             numPlayers = 2; // Dealer and human player
             // create new deck object first
             Deck newDeck = new Deck();
-            Hand playerHand = new Hand();
-            Hand dealerHand = new Hand();
-
-
         }
 
 
