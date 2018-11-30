1439a8e3ed48a9d5cda9a24f3f549c694c6cf7ed
Mbnhuy <mbnhuy@gmail.com>
2018-11-30 00:36:52 -0800

    Added a TotalChips method so, we can add up the total # of chips used for gambling

--- Blackjack/GambleChips.cs
+++ Blackjack/GambleChips.cs
@@ -5,16 +5,21 @@ using System.Text;
 using System.Threading.Tasks;
 namespace Blackjack
 {
-    class GambleChips
-    {
-        private string[] GambleTokens;
+	class GambleChips
+	{
+		private string GambleTokens;
 		private const int OneDollarChip = 1;
-        private const int FiveDollarChip = 5;
-        private const int TwentyFiveDollarChip = 25;
-        private const int FiftyDollarChip = 50;
-        private const int HundredDollarChip = 100;
+		private const int FiveDollarChip = 5;
+		private const int TwentyFiveDollarChip = 25;
+		private const int FiftyDollarChip = 50;
+		private const int HundredDollarChip = 100;
+
+		public TotalChips()
+		{
+			// TODO: Algorithm and code to add up total chips used
+		}
 
 
 
-    }
+	}
 }
