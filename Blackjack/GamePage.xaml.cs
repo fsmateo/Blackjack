b083b835ef313b554e71395ef3a76d7633b41a12
Carlo Hormoz <chormoz@sdsu.edu>
2018-12-01 10:34:53 -0800

    Added functioning menu bar to GamePage + some comments.

--- Blackjack/GamePage.xaml.cs
+++ Blackjack/GamePage.xaml.cs
@@ -27,5 +27,58 @@ namespace Blackjack
             this.InitializeComponent();
         }
 
+        /// <summary>
+        /// Returns to title screen from game page.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Return_Title(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage));
+        }
+
+        /// <summary>
+        /// Closes flyout from return to title screen if user cancels.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Close_Flyout(object sender, RoutedEventArgs e)
+        {
+            ReturnToTitleFlyout.Hide();
+        }
+
+        /// <summary>
+        /// Pressing settings button will bring up the options menu.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.IsPaneOpen = !Settings.IsPaneOpen;
+        }
+
+        /// <summary>
+        /// Toggles option one depending on which radio button is pressed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Toggle_Option_1(object sender, RoutedEventArgs e)
+        {
+            // TODO: FIGURE OUT OPTION 1 AND CHANGE NAME IN XAML
+        }
+
+        // TODO: KEEP OPTIONS CHECKED BETWEEN PAGE CHANGES.
+
+        /// <summary>
+        /// Toggles option two depending on which radio button is pressed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Toggle_Option_2(object sender, RoutedEventArgs e)
+        {
+            // TODO: FIGURE OUT OPTION 2 AND CHANGE NAME IN XAML
+        }
+
+
     }
 }
